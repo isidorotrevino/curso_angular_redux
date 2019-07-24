7ffe907bc56208fcac759b8a22f76aeae151b297
Isidoro <isidoro.trevino@vintec.mx>
2019-07-24 15:25:16 -0500

    Commit inicial

---
+++ ingresoEgresoApp/src/app/shared/fotter/fotter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fotter',
+  templateUrl: './fotter.component.html',
+  styles: []
+})
+export class FotterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
